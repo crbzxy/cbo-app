13c6997689214f2c35f1dc0aaaf1e78b7ff28046
Carlos Boyzo <carlos.boor@gmail.com>
2022-03-27 02:56:38 -0600

    ajuste de arte

--- src/Views/Art.js
+++ src/Views/Art.js
@@ -3,8 +3,14 @@ import React from 'react'
 function Art() {
   return (
     <section className="section">
-      <div className="contenedor"><h1>Arte</h1>
-        <iframe src="https://heuristic-swanson-bc515d.netlify.app/" title="metaverso" /></div>
+      <div className="contenedor">
+
+        <h1>Carlos Boyzo Oregon</h1>
+
+        <p>Artista Digital</p>
+
+      </div>
+
     </section>
   )
 }
