c5c9629dbec78cabdc258bed135dcd4d0982eec2
Carlos Boyzo <50590301+crbzxy@users.noreply.github.com>
2022-04-17 02:02:18 -0500

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { ExternalLink } from "react-external-link";
 import * as FaIcons from "react-icons/fa";
-import Logo from '../logo.svg';
+import Logo from '../logo.png';
 //import Logo from "../../img/logo.png";
 
 
